5ee407be411b460a4d53bd92e1591e5ef0580b94
codewizz <c.chaiwat.ch@gmail.com>
2021-05-23 13:43:08 +0700

    update: complete invent crud

--- backend/routers/inventorys.py
+++ backend/routers/inventorys.py
@@ -1,5 +1,7 @@
 import os, shutil
+from starlette.requests import Request
 from fastapi import APIRouter, UploadFile, File, Form, HTTPException
+from fastapi.encoders import jsonable_encoder
 from models.inventory import InventPydantic, InventPydanticIn, Inventory
 from models.utils import Status
 
@@ -17,12 +19,13 @@ async def save_file(image_name):
         else:
             return True
 
+
 router = APIRouter()
 
 
 @router.get("/")
 async def all_inventory():
-    return await InventPydantic.from_queryset(Inventory.all())
+    return await InventPydantic.from_queryset(Inventory.all().order_by('-id'))
 
 
 @router.get("/item/{item_id}", response_model=InventPydantic)
@@ -50,13 +53,32 @@ async def create_inventory(
 
 
 @router.put("/item/{item_id}")
-async def update_inventory(id: int):
-
-    return { 'id': id }
+async def update_inventory(item_id, request: Request):
+    form_data = await request.form()
+    if await Inventory.filter(id=item_id).exists():
+        if form_data.get('image_name'):
+            await Inventory.filter(id=item_id).update(
+                    name = form_data.get('name'),
+                    price = form_data.get('price'),
+                    stock = form_data.get('stock'),
+                    image_name = form_data.get('image_name').filename
+                )
+            await save_file(form_data.get('image_name'))
+        else:
+            await Inventory.filter(id=item_id).update(
+                name = form_data.get('name'),
+                price = form_data.get('price'),
+                stock = form_data.get('stock'),
+            )
+        return await InventPydantic.from_queryset_single(Inventory.get(id=item_id))
+    else:
+        raise HTTPException(status_code=404, detail=f'Id {item_id} not found')
 
 
 @router.delete(
-    "/item/{id}", response_model=Status, responses={404: {'model': HTTPNotFoundError}}
+    "/item/{id}",
+    response_model=Status,
+    responses={404: {'model': HTTPNotFoundError}}
 )
 async def delete_inventory(id: int):
     delete_count = await Inventory.filter(id=id).delete()
