5a3c506a55d0175f91ee40256cd12171d470be48
codewizz <c.chaiwat.ch@gmail.com>
2021-05-25 22:44:39 +0700

    update: BoardInfo, add: ImageBase

--- front-end/src/components/core/BoardInfo.vue
+++ front-end/src/components/core/BoardInfo.vue
@@ -51,76 +51,51 @@
 
       <div class="columns">
           <div class="column is-full">
-              <div class="card events-card">
-                  <header class="card-header">
-                      <p class="card-header-title">
-                          Events
-                      </p>
-                      <a href="#" class="card-header-icon" aria-label="more options">
-        <span class="icon">
-          <i class="fa fa-angle-down" aria-hidden="true"></i>
-        </span>
-      </a>
-                  </header>
-                  <div class="card-table">
-                      <div class="content">
-                          <table class="table is-fullwidth is-striped">
-                              <tbody>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                                  <tr>
-                                      <td width="5%"><i class="fa fa-bell-o"></i></td>
-                                      <td>Lorum ipsum dolem aire</td>
-                                      <td class="level-right"><a class="button is-small is-primary" href="#">Action</a></td>
-                                  </tr>
-                              </tbody>
-                          </table>
-                      </div>
-                  </div>
-                  <footer class="card-footer">
-                      <a href="#" class="card-footer-item">View All</a>
-                  </footer>
-              </div>
+            <table class="table is-striped is-fullwidth">
+            <thead class="has-text-centered">
+                <tr>
+                    <th v-for="column in columnsHeader" :key="column">{{ column }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in inventory" :key="item.id" class="has-text-centered">
+                    <td>{{ item.id }}</td>
+                    <td>
+                        <ImageBase :image_name="item.image_name" />
+                    </td>
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.price }}</td>
+                    <td>{{ item.stock }}</td>
+                </tr>
+            </tbody>
+            </table>
           </div>
       </div>
-
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import ImageBase from '@/components/core/ImageBase'
+
+export default {
+    data() {
+        return {
+            columnsHeader: ['ID', 'image', 'name', 'price', 'stock', 'Activity']
+        }
+    },
+    components: {
+        ImageBase
+    },
+    mounted() {
+        this.$store.dispatch('fetchInventory')
+    },
+    filters: {
+        getImage(image_name) {
+            return `http://localhost:8000/static/image/${image_name}`
+        }
+    },
+    computed:mapState(['inventory'])
+
+}
+</script>
---
+++ front-end/src/components/core/ImageBase.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="has-text-center">
+        <figure class="image is-64x64 is-inline-block">
+            <img :src="getImage">
+        </figure>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageBase',
+  props: {
+    image_name: String,
+  },
+    computed: {
+        getImage() {
+            return `http://localhost:8000/static/images/${this.image_name}`
+        }
+    }
+}
+</script>
